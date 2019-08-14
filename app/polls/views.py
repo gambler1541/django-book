78ed9f1f9738d878d4e4365d0dcb3c02b7e3ba58
pmb <gambler1541@gmail.com>
2019-08-14 14:55:21 +0900

    url 네임스페이스 지정

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Question
 
@@ -14,7 +14,15 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking ah question %s." %question_id)
+    # 파일이 존재하지 않을 때 발생하는 404오류
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+
+    # Http404를 처리할 때 loader-render 관계처럼(render) 단축 함수가 존재(get_object_or_404)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def result(request, question_id):
     response = "You're looking ah the results of queston %s."
