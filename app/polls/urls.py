6af00aed5884d75e9fdf9e94227af062e50722de
pmb <gambler1541@gmail.com>
2019-08-14 13:44:11 +0900

    Index view에 추가한 Question이 보이게 설정(HttpResponse)

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex) /polls/
     path('', views.index, name='index'),
+    # ex) /polls/1/
+    # <>괄호안에 해당하는 값을 뷰에 인자로 전달
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex) /polls/1/result/
+    path('<int:question_id>/result/', views.result, name='result'),
+    # ex) /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking ah question %s." %question_id)
+
+def result(request, question_id):
+    response = "You're looking ah the results of queston %s."
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're votiong on question %s" %question_id)
