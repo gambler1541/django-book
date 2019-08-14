9e13dfc487ddf60e777c74061e2119d91a186cee
pmb <gambler1541@gmail.com>
2019-08-14 14:06:26 +0900

    view와 templates분리

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # render 메서드는 request와 템플릿 이름, 사전형 객체(context)를 인자로 받음
+    #   사전형 객체는 템플릿에서 사용할 변수
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking ah question %s." %question_id)
