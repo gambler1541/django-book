eb879ceac8bc340727643e2b8b2a83032900f47f
pmb <gambler1541@gmail.com>
2019-08-14 13:12:11 +0900

    polls index 구현(localhost:8000/polls/

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include는 다른 urls.py 파일을 참조할 수 있도록 함
+    path('polls/', include('polls.urls')),
+
 ]
